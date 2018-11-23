1ed40fa98ca1f5ff4e1ce82b1de5def4b1546712
jallybally <43638098+jallybally@users.noreply.github.com>
2018-11-24 02:43:22 +0300

    Add files via upload

--- Scripts/color.js
+++ Scripts/color.js
@@ -1,34 +1,42 @@
-var color = GetCookie('color');
-function colorRed(){	
-	if (username != 'anonim') {
+
+function colorRed(){
+	var username = GetCookie('username');
+	if(username == 'red'){
 	document.body.style.color = '#FF0000';
 	document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';
 	document.getElementById('popup').style.borderColor = '#FF0000';
+	document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';
+	document.getElementById('alertm_all').style.borderColor = '#FF0000';
 	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_red.png?raw=true';
-	 document.cookie.SetCookie('red',color);
 	}
+	
 }
 function colorGreen(){
 	document.body.style.color = '#23d82f';
 	document.getElementById('tbr').style.borderBottom = '1px solid #23d82f';
 	document.getElementById('popup').style.borderColor = '#23d82f';
+	document.getElementById('alertm_all').style.borderColor = '#23d82f';
 	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_green.png?raw=true';
 }
 function colorBlue(){
 	document.body.style.color = '#00cedf';
 	document.getElementById('tbr').style.borderBottom = '1px solid #00cedf';
 	document.getElementById('popup').style.borderColor = '#00cedf';
+	document.getElementById('alertm_all').style.borderColor = '#00cedf';
 	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_blue.png?raw=true';
 }
 function colorYellow(){
 	document.body.style.color = '#f9fb09';
 	document.getElementById('tbr').style.borderBottom = '1px solid #f9fb09';
 	document.getElementById('popup').style.borderColor = '#f9fb09';
+	document.getElementById('alertm_all').style.borderColor = '#f9fb09';
 	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_yellow.png?raw=true';
 }
 function colorWhite(){
 	document.body.style.color = '#ffffff';
 	document.getElementById('tbr').style.borderBottom = '1px solid #ffffff';
 	document.getElementById('popup').style.borderColor = '#ffffff';
+	document.getElementById('alertm_all').style.borderColor = '#ffffff';
 	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_white.png?raw=true';
 }
+
--- Scripts/cookie.js
+++ Scripts/cookie.js
@@ -1,20 +1,19 @@
 function cookiest(){
-var username = GetCookie('username');
-var color = GetCookie('value');
-if (username == null) {
-	username = 'white';
+var color = GetCookie('username');
+if (color == null) {
+	color = 'white';
 	pathname = location.pathname;
 	myDomain = pathname.substring(0,pathname.lastIndexOf('/')) +'/';
 	var largeExpDate = new Date ();
 	largeExpDate.setTime(largeExpDate.getTime() + (3600 * 24 * 30));
-	SetCookie('username',username,largeExpDate,myDomain);
+	SetCookie('username',color,largeExpDate,myDomain);
 	alertMod('На сайте используется cookie');
 }
 function getCookieVal (offset) {
-   var endstr = document.cookie.indexOf (";", offset);
-   if (endstr == -1)
-      endstr = document.cookie.length;
-   return unescape(document.cookie.substring(offset, endstr));
+	var endstr = document.cookie.indexOf (";", offset);
+	if (endstr == -1)
+		endstr = document.cookie.length;
+	return unescape(document.cookie.substring(offset, endstr));
 }
 function GetCookie (name) {
         var arg = name + "=";
@@ -29,7 +28,7 @@ function GetCookie (name) {
                         if (i == 0)
                                 break;
                 }
-   return null;
+	return null;
 }
 function SetCookie (name, value) {
         var argv = SetCookie.arguments;
@@ -45,5 +44,71 @@ expires.toGMTString())) +
                 ((domain == null) ? "" : ("; domain=" + domain)) +
                 ((secure == true) ? "; secure" : "");
 }
-document.getElementById('user').innerHTML = username;
+/*document.getElementById('user').innerHTML = username;*/
+}
+/*alert*/
+function alertMod(content,afterFunction){
+	$('<div id="alertm_all">'+content+'<div class="alertm_but" onclick="alert_close('+afterFunction+'); return false">OK</div></div>').appendTo('body');
+	$(".overlay, .alertm_all").fadeIn("slow");
+}
+function alert_close(afterFunctionClouse){
+	$(".overlay, #alertm_all").remove();
+	afterFunctionClouse;
+}
+/*Color*/
+function colorRed(){
+	var color = GetCookie('username');
+	if(color != 'red'){
+	color = 'red';
+	document.body.style.color = '#FF0000';
+	document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';
+	document.getElementById('popup').style.borderColor = '#FF0000';
+	document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';
+	document.getElementById('alertm_all').style.borderColor = '#FF0000';
+	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_red.png?raw=true';
+	
+	function SetCookie (name, value) {
+        var argv = SetCookie.arguments;
+        var argc = SetCookie.arguments.length;
+        var expires = (argc > 2) ? argv[2] : null;
+        var path = (argc > 3) ? argv[3] : null;
+        var domain = (argc > 4) ? argv[4] : null;
+        var secure = (argc > 5) ? argv[5] : false;		
+        document.cookie = name + "=" + escape (value) +
+                ((expires == null) ? "" : ("; expires=" +
+				expires.toGMTString())) +
+                ((path == null) ? "" : ("; path=" + path)) +
+                ((domain == null) ? "" : ("; domain=" + domain)) +
+                ((secure == true) ? "; secure" : "");
+	}
+	}
+	
+}
+function colorGreen(){
+	document.body.style.color = '#23d82f';
+	document.getElementById('tbr').style.borderBottom = '1px solid #23d82f';
+	document.getElementById('popup').style.borderColor = '#23d82f';
+	document.getElementById('alertm_all').style.borderColor = '#23d82f';
+	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_green.png?raw=true';
+}
+function colorBlue(){
+	document.body.style.color = '#00cedf';
+	document.getElementById('tbr').style.borderBottom = '1px solid #00cedf';
+	document.getElementById('popup').style.borderColor = '#00cedf';
+	document.getElementById('alertm_all').style.borderColor = '#00cedf';
+	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_blue.png?raw=true';
+}
+function colorYellow(){
+	document.body.style.color = '#f9fb09';
+	document.getElementById('tbr').style.borderBottom = '1px solid #f9fb09';
+	document.getElementById('popup').style.borderColor = '#f9fb09';
+	document.getElementById('alertm_all').style.borderColor = '#f9fb09';
+	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_yellow.png?raw=true';
+}
+function colorWhite(){
+	document.body.style.color = '#ffffff';
+	document.getElementById('tbr').style.borderBottom = '1px solid #ffffff';
+	document.getElementById('popup').style.borderColor = '#ffffff';
+	document.getElementById('alertm_all').style.borderColor = '#ffffff';
+	document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_white.png?raw=true';
 }
