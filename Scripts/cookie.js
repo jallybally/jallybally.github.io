43bbb5cfd781733c1d30e2b9059a2899b996741a
jallybally <43638098+jallybally@users.noreply.github.com>
2018-11-24 03:31:58 +0300

    Add files via upload

--- Scripts/cookie.js
+++ Scripts/cookie.js
@@ -9,13 +9,18 @@ function cookiest(){
 		SetCookie('username',color,largeExpDate,myDomain);
 		alertMod('На сайте используется cookie');
 	}
-	else {
+	else if(color == 'red') {
 		document.body.style.color = '#FF0000';
 		document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';
 		document.getElementById('popup').style.borderColor = '#FF0000';
-		document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';
-		document.getElementById('alertm_all').style.borderColor = '#FF0000';
+		document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';		
 		document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_red.png?raw=true';
+		if (document.getElementById('alertm_all') == null){
+			
+		}
+		else{
+			document.getElementById('alertm_all').style.borderColor = '#FF0000';
+		}
 	}
 function getCookieVal (offset) {
 	var endstr = document.cookie.indexOf (";", offset);
