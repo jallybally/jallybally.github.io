60eef89896646e6d71a1f43804183b4ee0485e75
jallybally <43638098+jallybally@users.noreply.github.com>
2018-12-09 03:36:30 +0300

    Update color.js

--- Scripts/color.js
+++ Scripts/color.js
@@ -1,17 +1,116 @@
 function colorRed(){
-	alert('ну пиздец');
+	var color = GetCookie('username');
+			color = 'red';
+			pathname = location.pathname;
+			myDomain = pathname.substring(0,pathname.lastIndexOf('/')) +'/';
+			var largeExpDate = new Date ();
+			largeExpDate.setTime(largeExpDate.getTime() + (3600 * 24 * 30));
+			SetCookie('username',color,largeExpDate,myDomain);
+
+			document.body.style.color = '#FF0000';
+			document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';
+			document.getElementById('popup').style.borderColor = '#FF0000';
+			document.getElementById('tbr').style.borderBottom = '1px solid #FF0000';
+			document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_red.png?raw=true';
+			document.getElementById('alertm_all').style.borderColor = '#FF0000';
+	}
+	function getCookieVal (offset) {
+		var endstr = document.cookie.indexOf (";", offset);
+		if (endstr == -1)
+			endstr = document.cookie.length;
+		return unescape(document.cookie.substring(offset, endstr));
+	}
+	function GetCookie (name) {
+			var arg = name + "=";
+			var alen = arg.length;
+			var clen = document.cookie.length;
+			var i = 0;
+			while (i < clen) {
+					var j = i + alen;
+					if (document.cookie.substring(i, j) == arg)
+							return getCookieVal (j);
+					i = document.cookie.indexOf(" ", i) + 1;
+							if (i == 0)
+									break;
+					}
+		return null;
+	}
+	function SetCookie (name, value) {
+			var argv = SetCookie.arguments;
+			var argc = SetCookie.arguments.length;
+			var expires = (argc > 2) ? argv[2] : null;
+			var path = (argc > 3) ? argv[3] : null;
+			var domain = (argc > 4) ? argv[4] : null;
+			var secure = (argc > 5) ? argv[5] : false;		
+			document.cookie = name + "=" + escape (value) +
+					((expires == null) ? "" : ("; expires=" +
+	expires.toGMTString())) +
+					((path == null) ? "" : ("; path=" + path)) +
+					((domain == null) ? "" : ("; domain=" + domain)) +
+					((secure == true) ? "; secure" : "");
+	}
+}
+function colorGreen(){
+	 var color = GetCookie('username');
+			color = 'green';
+			pathname = location.pathname;
+			myDomain = pathname.substring(0,pathname.lastIndexOf('/')) +'/';
+			var largeExpDate = new Date ();
+			largeExpDate.setTime(largeExpDate.getTime() + (3600 * 24 * 30));
+			SetCookie('username',color,largeExpDate,myDomain);
+
+			document.body.style.color = '#23d82f';
+			document.getElementById('tbr').style.borderBottom = '1px solid #23d82f';
+			document.getElementById('popup').style.borderColor = '#23d82f';
+			document.getElementById('popupIMG').src = 'https://github.com/jallybally/jallybally.github.io/blob/master/css/Resources/gear_green.png?raw=true';
+			document.getElementById('alertm_all').style.borderColor = '#23d82f';
+
+	}
+	function getCookieVal (offset) {
+		var endstr = document.cookie.indexOf (";", offset);
+		if (endstr == -1)
+			endstr = document.cookie.length;
+		return unescape(document.cookie.substring(offset, endstr));
+	}
+	function GetCookie (name) {
+			var arg = name + "=";
+			var alen = arg.length;
+			var clen = document.cookie.length;
+			var i = 0;
+			while (i < clen) {
+					var j = i + alen;
+					if (document.cookie.substring(i, j) == arg)
+							return getCookieVal (j);
+					i = document.cookie.indexOf(" ", i) + 1;
+							if (i == 0)
+									break;
+					}
+		return null;
+	}
+	function SetCookie (name, value) {
+			var argv = SetCookie.arguments;
+			var argc = SetCookie.arguments.length;
+			var expires = (argc > 2) ? argv[2] : null;
+			var path = (argc > 3) ? argv[3] : null;
+			var domain = (argc > 4) ? argv[4] : null;
+			var secure = (argc > 5) ? argv[5] : false;		
+			document.cookie = name + "=" + escape (value) +
+					((expires == null) ? "" : ("; expires=" +
+	expires.toGMTString())) +
+					((path == null) ? "" : ("; path=" + path)) +
+					((domain == null) ? "" : ("; domain=" + domain)) +
+					((secure == true) ? "; secure" : "");
+	}
 }
 function colorBlue(){
 	 var color = GetCookie('username');
-		if (color != 'blue') {
 			color = 'blue';
 			pathname = location.pathname;
 			myDomain = pathname.substring(0,pathname.lastIndexOf('/')) +'/';
 			var largeExpDate = new Date ();
 			largeExpDate.setTime(largeExpDate.getTime() + (3600 * 24 * 30));
 			SetCookie('username',color,largeExpDate,myDomain);
-		}
-		else {
+
 			document.body.style.color = '#00cedf';
 			document.getElementById('tbr').style.borderBottom = '1px solid #00cedf';
 			document.getElementById('popup').style.borderColor = '#00cedf';
@@ -22,7 +121,7 @@ function colorBlue(){
 			else{
 				document.getElementById('alertm_all').style.borderColor = '#00cedf';
 			}
-		}
+	}
 	function getCookieVal (offset) {
 		var endstr = document.cookie.indexOf (";", offset);
 		if (endstr == -1)
@@ -61,15 +160,13 @@ function colorBlue(){
 }
 function colorYellow(){
 	 var color = GetCookie('username');
-		if (color != 'yellow') {
 			color = 'yellow';
 			pathname = location.pathname;
 			myDomain = pathname.substring(0,pathname.lastIndexOf('/')) +'/';
 			var largeExpDate = new Date ();
 			largeExpDate.setTime(largeExpDate.getTime() + (3600 * 24 * 30));
 			SetCookie('username',color,largeExpDate,myDomain);
-		}
-		else {
+
 			document.body.style.color = '#f9fb09';
 			document.getElementById('tbr').style.borderBottom = '1px solid #f9fb09';
 			document.getElementById('popup').style.borderColor = '#f9fb09';
@@ -80,7 +177,7 @@ function colorYellow(){
 			else{
 				document.getElementById('alertm_all').style.borderColor = '#f9fb09';
 			}
-		}
+	}
 	function getCookieVal (offset) {
 		var endstr = document.cookie.indexOf (";", offset);
 		if (endstr == -1)
@@ -119,15 +216,13 @@ function colorYellow(){
 }
 function colorWhite(){
 	 var color = GetCookie('username');
-		if (color != 'white') {
 			color = 'white';
 			pathname = location.pathname;
 			myDomain = pathname.substring(0,pathname.lastIndexOf('/')) +'/';
 			var largeExpDate = new Date ();
 			largeExpDate.setTime(largeExpDate.getTime() + (3600 * 24 * 30));
 			SetCookie('username',color,largeExpDate,myDomain);
-		}
-		else {
+			
 			document.body.style.color = '#ffffff';
 			document.getElementById('tbr').style.borderBottom = '1px solid #ffffff';
 			document.getElementById('popup').style.borderColor = '#ffffff';
@@ -138,7 +233,7 @@ function colorWhite(){
 			else{
 				document.getElementById('alertm_all').style.borderColor = '#ffffff';
 			}
-		}
+	}
 	function getCookieVal (offset) {
 		var endstr = document.cookie.indexOf (";", offset);
 		if (endstr == -1)
